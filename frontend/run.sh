9bf4aef691d5918b57cbd61c0d5d7f9a88206968
Rui Miguel <ruimiguel@fragmentized.net>
2020-06-19 01:13:52 +0100

    Enable Cron Daemon on Frontend Container
    Let's Encrypt certificates expire every 3 months. In order to issue
    renewal requests crond was enabled on the container along with a cronjob
    set to run every week.
    
    Added two new environment variables that when enabled trigger Let's
    Encrypt ativation: EMAIL and DOMAINS

--- frontend/run.sh
+++ frontend/run.sh
@@ -8,25 +8,30 @@ trap "exit" INT TERM
 
 domains=${DOMAINS:-localhost}
 
+get_domain_flags() {
+    local domain_flags=""
+
+    for domain in $domains; do
+        domain_flags="$domain_flags -d $domain"
+    done
+
+    echo "$domain_flags"
+}
+
 replace() {
     sed -i "s~<DOMAINS>~$domains~g" /etc/nginx/conf.d/default.conf
     sed -i "s~<SSL_PATH>~$SSL_PATH~g" /etc/nginx/conf.d/default.conf
 }
 
 run_lego() {
-    local domain_flags=""
     local first_domain=$(printf "%s" "${domains%% *}")
-
-    for domain in $domains; do
-        domain_flags="$domain_flags -d $domain"
-    done
 
     lego --http.webroot /app \
         --email $EMAIL \
         --http \
         --accept-tos \
         --path /etc/ssl/lego \
-        $domain_flags \
+        $(get_domain_flags) \
         run
 
     rm "${SSL_PATH}"/app.key
@@ -35,11 +40,19 @@ run_lego() {
     ln -s /etc/ssl/lego/certificates/"${first_domain}".key "${SSL_PATH}"/app.key
 }
 
+setup_cron() {
+    echo "0 0 * * 0 lego --http.webroot /app --email $EMAIL --http --path /etc/ssl/lego $(get_domain_flags) renew >> /var/log/lego.log && /usr/sbin/nginx -s reload" > /etc/crontabs/lego
+    crontab /etc/crontabs/lego
+    crond
+}
+
 replace && nginx -g "daemon off;" &
 nginx_pid=$!
 
 if [[ "$domains" != "localhost"  ]]; then
     run_lego
+    setup_cron
 fi
 
+nginx -s reload
 wait $nginx_pid
